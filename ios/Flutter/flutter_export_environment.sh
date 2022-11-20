5deb163d34c576ff52ffd29ac08a6c0296086797
Rengga Firmandika <renggafirmandika@gmail.com>
2022-11-20 15:13:45 +0700

    perbaikan db

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\Rengga\Master\Flutter\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Rengga\Akar Bahar\akar_bahar\akar_bahar"
+export "FLUTTER_ROOT=C:\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\Rengga\Kantor\Akar Bahar\App\akar_bahar"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NAME=1.1.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
